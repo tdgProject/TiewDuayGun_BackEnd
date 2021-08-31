b4be16e3520e373f555fe9fea68837bf31302edc
Attapon Jeamjumroensuk <attapon.pan@mail.kmutt.ac.th>
2021-08-31 15:30:30 +0700

    First Initialize Backend Project

---
+++ src/test/java/int222/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package int222.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
