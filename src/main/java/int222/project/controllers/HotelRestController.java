253a31e8ba92fcc23865563801d87f413ee25116
Attapon Jeamjumroensuk <attapon.pan@mail.kmutt.ac.th>
2021-10-12 13:58:45 +0700

    [Upadate] Hotel CRUD

--- src/main/java/int222/project/controllers/HotelRestController.java
+++ src/main/java/int222/project/controllers/HotelRestController.java
@@ -3,10 +3,10 @@ package int222.project.controllers;
 import int222.project.models.*;
 import int222.project.repositories.HotelRepository;
 import int222.project.repositories.NearByRepository;
-import int222.project.repositories.ReviewRepository;
+import int222.project.repositories.PlaceRepository;
+import int222.project.repositories.UserRepository;
 import int222.project.services.FileService;
 import int222.project.services.HotelFileService;
-import int222.project.services.PlaceFileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
@@ -24,22 +24,65 @@ public class HotelRestController {
     private HotelRepository hotelRepository;
     @Autowired
     private NearByRepository nearByRepository;
+    @Autowired
+    private PlaceRepository placeRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     private FileService fileService = new HotelFileService();
 
     @GetMapping("/hotels")
-    public List<NearBy> listHotel(){
-        return nearByRepository.findAll();
+    public List<Hotel> listAllHotel(){
+        return hotelRepository.findAll();
     }
 
     @GetMapping("/hotel/{id}")
     public List<NearBy> listHotelByPlaceId(@PathVariable int id) {
         return nearByRepository.findAllByPlaceId(id);
     }
+
     @GetMapping("/image/hotel/{name}")
     @ResponseBody
     public ResponseEntity<org.springframework.core.io.Resource> getHotelImage(@PathVariable String name) {
         org.springframework.core.io.Resource file = (Resource) fileService.loadAsResource(name);
         return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(file);
+    }
+
+    @PostMapping(value = "/hotel/add", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public Hotel addHotel(@RequestParam(value = "image", required = false) MultipartFile hotelImage, @RequestPart Hotel newHotel) {
+        if(hotelImage != null) {
+            newHotel.setImage(fileService.save(hotelImage, newHotel.getHotelName()));
+        }
+        User user = userRepository.findById(newHotel.getOwner().getUserId()).orElse(null);
+        newHotel.setOwner(user);
+        return hotelRepository.saveAndFlush(newHotel);
+    }
+
+    @PutMapping(value = "/hotel/edit/{id}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public Hotel editHotel(@RequestParam(value = "image", required = false) MultipartFile hotelImage,@RequestPart Hotel newHotel,@PathVariable int id) {
+        Hotel h = hotelRepository.findById(id).orElse(null);
+        h.setHotelName(newHotel.getHotelName());
+        h.setAddress(newHotel.getAddress());
+        h.setEmail(newHotel.getEmail());
+        h.setTelNumber(newHotel.getTelNumber());
+        if(hotelImage != null) {
+            fileService.delete(h.getImage());
+            h.setImage(fileService.save(hotelImage, h.getHotelName()));
+        }
+        return hotelRepository.saveAndFlush(h);
+    }
+
+    @PostMapping(value = "/nearby/add/{id}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public NearBy addNearby(@RequestPart NearBy newNearBy,@PathVariable int id) throws Exception {
+        newNearBy.setNearById(new NearByPK(id,newNearBy.getHotel().getHotelId()));
+        newNearBy.setHotel(hotelRepository.findById(newNearBy.getHotel().getHotelId()).orElse(null));
+        newNearBy.setPlace(placeRepository.findById(id).orElse(null));
+        return nearByRepository.saveAndFlush(newNearBy);
+    }
+
+    @DeleteMapping("/nearby/delete/{pid}/{hid}")
+    public void deleteNearBy(@PathVariable int pid,@PathVariable int hid) {
+        nearByRepository.deleteById(new NearByPK(pid,hid));
+        nearByRepository.flush();
     }
 }
--- src/main/java/int222/project/repositories/HotelRepository.java
+++ src/main/java/int222/project/repositories/HotelRepository.java
@@ -1,6 +1,5 @@
 package int222.project.repositories;
 
-import int222.project.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import int222.project.models.Hotel;
 import org.springframework.data.jpa.repository.Modifying;
@@ -8,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 
 public interface HotelRepository extends JpaRepository<Hotel, Integer> {
+
 }
